29f7e8273f59538dfefdafaa09839d2bcaf19a85
pmurph0305 <pmurph0305@gmail.com>
2019-06-06 17:18:25 -0400

    Finished FCC Markdown preview project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Editor from './Editor/Editor';
 import Preview from './Preview/Preview';
 
+  /* User Story #5: When my markdown previewer first loads, 
+  the default text in the #editor field should contain valid markdown that represents 
+  at least one of each of the following elements: 
+  a header (H1 size), 
+  a sub header (H2 size),
+   a link, 
+   inline code, 
+   a code block, 
+   a list item, 
+   a blockquote,
+    an image, 
+    and bolded text.
+  */
+
+const initialExample = `# Welcome to the markdown editor!
+  ## Enter markdown in the editor to see the preview below!
+  ### You can do all kinds of headings...
+  You can also *emphasize* or **bold** things with asterisks.  
+  You can put links in: [Like this one to FreeCodeCamp!](https://freecodecamp.org)
+  \`inline code can also be written\` between backticks
+  \`\`\`
+  code blocks
+  can also
+  be written here
+   \`\`\`
+  >block quotes can come in handy too.
+
+  1. lists can be made
+  1. with numbers
+
+  or
+  * unordered lists
+  * can be made
+  * with asterisks
+  You could even put images in if you want like this: ![image of a dog](https://i.pinimg.com/originals/4e/ae/65/4eae65b560cfad428d1874af80835451.jpg)
+`;
+
 class App extends React.Component {
+
+
+
 
   constructor(props) {
     super(props);
     this.state = {
-      text: '# Welcome to the markdown editor!'
+      text: initialExample
     }
   }
 
--- src/Preview/Preview.js
+++ src/Preview/Preview.js
@@ -1,14 +1,33 @@
 import React from 'react';
-
+import marked from 'marked';
 import "./Preview.css"
 
 const Preview = ({text}) => {
+
+  // Optional Bonus (you do not need to make this test pass): When I click a link rendered by my markdown previewer, 
+  //the link is opened up in a new tab (HINT: read the Marked.js docs for this one!).
+  let renderer = new marked.Renderer();
+  renderer.link = function (href, title, text) {
+    return `<a href=${href} target="_blank">${text}</a>`
+  }
+
+ 
+
+  // User Story #4: When I enter GitHub flavored markdown into the #editor element, the text is rendered as HTML in the #preview element as I type 
+  const getMarkup = () => {
+    // Optional Bonus (you do not need to make this test pass): My markdown previewer interprets carriage returns and renders them as br (line break) elements.
+    // done by replacing \r and \n with "  \n"
+    let newLines = text.replace(/\n|\r|\r\n/gim, "  \n");
+    return{__html: marked(newLines, {renderer: renderer})}
+  }
+
   return (
     <div className="preview-container">
       <label htmlFor="preview" className="preview-label">Markdown Preview</label>
       {/* User Story #2: I can see an element with a corresponding id="preview". */}
-      {/* User Story #3: When I enter text into the #editor element, the #preview element is updated as I type to display the content of the textarea. */}
-      <div id="preview" className="preview-display">{text}</div>
+  
+       <div id="preview" className="preview-markdown" dangerouslySetInnerHTML={getMarkup()}></div>
+ 
     </div>
   )
 }
